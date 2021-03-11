4bf6bbe1abc5470bb0685cbad4031e99b17f73b0
Sindie Castro <Sindie Castro>
2021-03-10 18:36:26 -0600

    adding PyPoll_Challenge

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -104,7 +104,7 @@ with open(file_to_save, "w") as txt_file:
         print(county_results) 
 
         # 6e: Save the county votes to a text file.
-        # txt_file.write(county_results)
+        txt_file.write(county_results)
 
         # 6f: Write an if statement to determine the winning county and get its vote count. 
         if (winning_county_votes > county_total_votes):
